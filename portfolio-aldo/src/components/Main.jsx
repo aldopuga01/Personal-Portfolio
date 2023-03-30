8dc5258000d7bc6aa371cfb3f333321bec009937
aldopuga01 <56364832+aldopuga01@users.noreply.github.com>
2023-03-29 23:13:17 -0600

    Inicio de principal

---
+++ portfolio-aldo/src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div id='main'>
+            <img className='w-full h-screen object-cover'
+            src='https://images.unsplash.com/photo-1658708818685-b524d9abd4f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' 
+            alt='Fondo de página: Fotografía por Jandra Sutton en Unsplash https://unsplash.com/es/@jandralee' 
+            />
+            <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Main;
