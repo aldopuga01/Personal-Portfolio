5d8e883f1eacb6eaac3c653255c9a64692e1fb75
aldopuga01 <56364832+aldopuga01@users.noreply.github.com>
2023-03-29 23:13:01 -0600

    Sidenav completo

--- portfolio-aldo/src/components/Sidenav.jsx
+++ portfolio-aldo/src/components/Sidenav.jsx
@@ -1,12 +1,12 @@
-import React, {useState} from "react";
+import React, {useState} from 'react';
 import { 
     AiOutlineMenu, 
     AiOutlineHome, 
     AiOutlineHistory, 
     AiOutlineProject,
     AiOutlineMail
-} from "react-icons/ai";
-import { BsPerson } from "react-icons/bs";
+} from 'react-icons/ai';
+import { BsPerson } from 'react-icons/bs';
 
 
 const Sidenav = () => {
@@ -18,43 +18,43 @@ const Sidenav = () => {
 
     return (
         <div>
-            <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden' />
+            <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden transition-all ease-in-out duration-500' />
             {
                 nav ? (
-                    <div className='fixed w-full h-screen bg-gradient-to-b from-indigo-600 to-blue-400 flex flex-col justify-center items-center z-20'> 
+                    <div className='fixed w-full h-screen bg-gradient-to-b flex flex-col justify-center items-center z-20'> 
                         <a 
-                            href="#main" 
+                            href='#main' 
                             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-white shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                         >
-                            <AiOutlineHome size={20} className="text-indigo-600"/>
+                            <AiOutlineHome size={20} className='text-indigo-600'/>
                             <span className='pl-4 text-black'>Home</span>
                         </a>
                         <a 
-                            href="#main" 
+                            href='#history' 
                             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-white shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                         >
-                            <AiOutlineHistory size={20} className="text-indigo-600"/>
+                            <AiOutlineHistory size={20} className='text-indigo-600'/>
                             <span className='pl-4 text-black'>Experience</span>
                         </a>
                         <a 
-                            href="#main" 
+                            href='#projects' 
                             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-white shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                         >
-                            <AiOutlineProject size={20} className="text-indigo-600"/>
+                            <AiOutlineProject size={20} className='text-indigo-600'/>
                             <span className='pl-4 text-black'>Projects</span>
                         </a>
                         <a 
-                            href="#main" 
+                            href='#main' 
                             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-white shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                         >
-                            <BsPerson size={20} className="text-indigo-600"/>
+                            <BsPerson size={20} className='text-indigo-600'/>
                             <span className='pl-4 text-black'>Resume</span>
                         </a>
                         <a 
-                            href="#main" 
+                            href='#contact' 
                             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-white shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                         >
-                            <AiOutlineMail size={20} className="text-indigo-600"/>
+                            <AiOutlineMail size={20} className='text-indigo-600'/>
                             <span className='pl-4 text-black'>Contact</span>
                         </a>
                     </div>
@@ -65,29 +65,29 @@ const Sidenav = () => {
             <div className='md:block hidden fixed top-[25%] z-10'>
                 <div className='flex flex-col'>
                     <a 
-                        href="#main" 
+                        href='#main' 
                         className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                        <AiOutlineHome size={20} className="text-indigo-600"/>
+                        <AiOutlineHome size={20} className='text-indigo-600'/>
                     </a>
                     <a 
-                        href="#main" 
+                        href='#history' 
                         className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                        <AiOutlineHistory size={20} className="text-indigo-600"/>
+                        <AiOutlineHistory size={20} className='text-indigo-600'/>
                     </a>
                     <a 
-                        href="#main" 
+                        href='#projects' 
                         className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                        <AiOutlineProject size={20} className="text-indigo-600"/>
+                        <AiOutlineProject size={20} className='text-indigo-600'/>
                     </a>
                     <a 
-                        href="#main" 
+                        href='#main' 
                         className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                        <BsPerson size={20} className="text-indigo-600"/>
+                        <BsPerson size={20} className='text-indigo-600'/>
                     </a>
                     <a 
-                        href="#main" 
+                        href='#contact' 
                         className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                        <AiOutlineMail size={20} className="text-indigo-600"/>
+                        <AiOutlineMail size={20} className='text-indigo-600'/>
                     </a>
                 </div>
             </div>
