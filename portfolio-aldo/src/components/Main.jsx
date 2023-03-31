7d13a1dc057ce137c2e785f44301863c9dd11942
aldopuga01 <56364832+aldopuga01@users.noreply.github.com>
2023-03-30 18:24:28 -0600

    Actualizaciones
    Actualizaciones de tamaños, finalización del main y agregado de svg para el símbolo de ventana en navegador web.

--- portfolio-aldo/src/components/Main.jsx
+++ portfolio-aldo/src/components/Main.jsx
@@ -6,21 +6,21 @@ const Main = () => {
     return (
         <div id='main'>
             <img className='w-full h-screen object-cover'
-            src='https://images.unsplash.com/photo-1658708818685-b524d9abd4f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80' 
-            alt='Fondo de página: Fotografía por Jandra Sutton en Unsplash https://unsplash.com/es/@jandralee' 
+            src='https://images.unsplash.com/photo-1503756234508-e32369269deb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80' 
+            alt='Fondo de página: Fotografía por Kees Streefkerk kees_streefkerk en Unsplash https://unsplash.com/es/@kees_streefkerk' 
             />
             <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
                 <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
-                    <h1>Soy Aldo</h1>
+                    <h1 className='sm:text-5xl text-3xl font-semibold text-gray-800 pb-3'>Soy Aldo</h1>
                     <h2>
-                        <span>Desarrollador Web Jr. </span>
-                        <span>
+                        <span className='sm:text-3xl text-2xl font-semibold text-gray-800 pt-6'>Desarrollador Web Jr. </span>
+                        <span className='sm:text-3xl text-2xl font-semibold text-gray-800 pt-6'>
                             <TypeAnimation
                             sequence={[
                                 "FrontEnd", // Types 'One'
-                                1000, // Waits 1s
+                                3000, // Waits 1s
                                 "BackEnd", // Deletes 'One' and types 'Two'
-                                2000, // Waits 2s
+                                3000, // Waits 2s
                             ]}
                             wrapper="span"
                             cursor={true}
@@ -28,15 +28,15 @@ const Main = () => {
                             />
                         </span>
                     </h2>
-                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <div className='flex justify-between pt-6 max-w-[200px]' >
                         <a href='https://github.com/aldopuga01' target='_blank' rel='noopener noreferrer'>
-                            <FaGithub className='cursor-pointer' size={25} style={{ marginRight: '10px' }} />
+                            <FaGithub className='cursor-pointer' size={35} style={{ marginRight: '10px' }} />
                         </a>
                         <a href='https://www.linkedin.com/in/aldo-puga-vega-7b4201185/' target='_blank' rel='noopener noreferrer'>
-                            <FaLinkedin className='cursor-pointer' size={25} style={{ marginRight: '10px' }} />
+                            <FaLinkedin className='cursor-pointer' size={35} style={{ marginRight: '10px' }} />
                         </a>
                         <a href='https://www.instagram.com/pugaldo01/' target='_blank' rel='noopener noreferrer'>
-                            <FaInstagram className='cursor-pointer' size={25} style={{ marginRight: '10px' }} />
+                            <FaInstagram className='cursor-pointer' size={35} style={{ marginRight: '10px' }} />
                         </a>
                     </div>
                 </div>
--- portfolio-aldo/src/components/Sidenav.jsx
+++ portfolio-aldo/src/components/Sidenav.jsx
@@ -18,7 +18,7 @@ const Sidenav = () => {
 
     return (
         <div>
-            <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden transition-all ease-in-out duration-500' />
+            <AiOutlineMenu onClick={handleNav} className='w-6 h-6 absolute top-4 right-4 z-[99] md:hidden transition-all ease-in-out duration-500' />
             {
                 nav ? (
                     <div className='fixed w-full h-screen bg-gradient-to-b flex flex-col justify-center items-center z-20'> 
@@ -26,35 +26,35 @@ const Sidenav = () => {
                             href='#main' 
                             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-white shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                         >
-                            <AiOutlineHome size={20} className='text-indigo-600'/>
+                            <AiOutlineHome size={20} className='text-gray-800'/>
                             <span className='pl-4 text-black'>Home</span>
                         </a>
                         <a 
                             href='#history' 
                             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-white shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                         >
-                            <AiOutlineHistory size={20} className='text-indigo-600'/>
+                            <AiOutlineHistory size={20} className='text-gray-800'/>
                             <span className='pl-4 text-black'>Experience</span>
                         </a>
                         <a 
                             href='#projects' 
                             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-white shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                         >
-                            <AiOutlineProject size={20} className='text-indigo-600'/>
+                            <AiOutlineProject size={20} className='text-gray-800'/>
                             <span className='pl-4 text-black'>Projects</span>
                         </a>
                         <a 
                             href='#main' 
                             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-white shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                         >
-                            <BsPerson size={20} className='text-indigo-600'/>
+                            <BsPerson size={20} className='text-gray-800'/>
                             <span className='pl-4 text-black'>Resume</span>
                         </a>
                         <a 
                             href='#contact' 
                             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-white shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
                         >
-                            <AiOutlineMail size={20} className='text-indigo-600'/>
+                            <AiOutlineMail size={20} className='text-gray-800'/>
                             <span className='pl-4 text-black'>Contact</span>
                         </a>
                     </div>
@@ -67,27 +67,27 @@ const Sidenav = () => {
                     <a 
                         href='#main' 
                         className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                        <AiOutlineHome size={20} className='text-indigo-600'/>
+                        <AiOutlineHome size={20} className='text-gray-800'/>
                     </a>
                     <a 
                         href='#history' 
                         className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                        <AiOutlineHistory size={20} className='text-indigo-600'/>
+                        <AiOutlineHistory size={20} className='text-gray-800'/>
                     </a>
                     <a 
                         href='#projects' 
                         className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                        <AiOutlineProject size={20} className='text-indigo-600'/>
+                        <AiOutlineProject size={20} className='text-gray-800'/>
                     </a>
                     <a 
                         href='#main' 
                         className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                        <BsPerson size={20} className='text-indigo-600'/>
+                        <BsPerson size={20} className='text-gray-800'/>
                     </a>
                     <a 
                         href='#contact' 
                         className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300 '>
-                        <AiOutlineMail size={20} className='text-indigo-600'/>
+                        <AiOutlineMail size={20} className='text-gray-800'/>
                     </a>
                 </div>
             </div>
